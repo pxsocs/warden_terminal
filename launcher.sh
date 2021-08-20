e52966f891f89e1d2e9dd705976c279dbe922931
Alpha Zeta <alphazeta@protonmail.com>
2021-08-20 10:52:21 -0300

    Testing auto start at boot

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,5 @@
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
-
-cd ~
-cd /home/admin/warden_terminal
+#!/bin/bash
+cd /home/pi/coding/warden_terminal
 python3 node_warden.py
-cd ~
 
 
